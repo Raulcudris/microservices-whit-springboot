6867944940a9d9b1fccae425d70b1ec3c2b72f15
RaulCudris <raulcudriz@hotmail.com>
2024-06-14 11:51:59 -0500

    keycloak 3

--- users-services/src/main/java/com/makiia/users_services/security/UserSecurity.java
+++ users-services/src/main/java/com/makiia/users_services/security/UserSecurity.java
@@ -1,4 +1,5 @@
 package com.makiia.users_services.security;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
