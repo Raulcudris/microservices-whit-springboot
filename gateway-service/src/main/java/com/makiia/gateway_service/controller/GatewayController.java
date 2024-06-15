f6bde892541e2b33e053e5fbd77712792861bd16
RaulCudris <raulcudriz@hotmail.com>
2024-06-15 11:19:01 -0500

    keycloak 7

--- gateway-service/src/main/java/com/makiia/gateway_service/controller/GatewayController.java
+++ gateway-service/src/main/java/com/makiia/gateway_service/controller/GatewayController.java
@@ -1,5 +1,4 @@
 package com.makiia.gateway_service.controller;
-
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
