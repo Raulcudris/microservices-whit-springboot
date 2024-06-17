e7c35b4491cd7267de92db783a46f033404a199f
RaulCudris <raulcudriz@hotmail.com>
2024-06-17 12:42:15 -0500

    Actualizacion de cambios

--- auth-service/src/main/java/com/makiia/auth_services/AuthServiceApplication.java
+++ auth-service/src/main/java/com/makiia/auth_services/AuthServiceApplication.java
@@ -1,9 +1,10 @@
 package com.makiia.auth_services;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
