9d49150b3b27b3055f24c80708e84d67c0f8ef19
RaulCudris <raulcudriz@hotmail.com>
2024-08-06 09:46:44 -0500

    auth-jwt update

--- bikes-services/src/main/java/com/makiia/bikes_services/BikesServicesApplication.java
+++ bikes-services/src/main/java/com/makiia/bikes_services/BikesServicesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class BikesServicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BikesServicesApplication.class, args);
 	}
--- gateway-service/src/main/java/com/makiia/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/makiia/gateway_service/GatewayServiceApplication.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
